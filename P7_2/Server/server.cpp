fe1f18cbbdebb1e27a0943670517c5eebe0affe3
Timothy Luciani <tbl8@pitt.edu>
2011-04-18 20:38:58 -0400

    added custom float4

--- P7_2/Server/server.cpp
+++ P7_2/Server/server.cpp
@@ -1,13 +1,3 @@
-//
-// server.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
@@ -17,7 +7,7 @@
 #include <boost/serialization/vector.hpp>
 #include <fstream>
 
-#include "Mesh/Mesh.hh"
+#include "Mesh/Mesh.hh" 
 
 namespace s11n_example {
 

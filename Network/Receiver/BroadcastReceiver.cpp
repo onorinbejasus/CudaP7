ffc0a87dba20fb672a1e079d319e76516cba1f21
Timothy Luciani <tbl8@pitt.edu>
2011-04-20 03:21:43 -0400

    working on network stuff

---
+++ Network/Receiver/BroadcastReceiver.cpp
@@ -0,0 +1,70 @@
+/*
+ *   C++ sockets on Unix and Windows
+ *   Copyright (C) 2002
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "PracticalSocket.h"   // For UDPSocket and SocketException
+#include <iostream>            // For cout and cerr
+#include <cstdlib>             // For atoi()
+#include "data.hh"
+
+const int MAXRCVSTRING = 4096; // Longest string to receive
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {                  // Test for correct number of parameters
+    cerr << "Usage: " << argv[0] << " <Local Port>" << endl;
+    exit(1);
+  }
+
+  unsigned short echoServPort = atoi(argv[1]);     // First arg:  local port
+
+  try {
+    UDPSocket sock(echoServPort);                
+  
+    // char recvString[MAXRCVSTRING + 1]; // Buffer for echo string + \0
+    int recvNum;
+    string sourceAddress;              // Address of datagram source
+    unsigned short sourcePort;         // Port of datagram source
+
+//    while(true)
+//    {
+		
+        int bytesRcvd = sock.recvFrom(&recvNum, 4, sourceAddress, 
+                                  sourcePort);
+        
+		struct m_triangle *mesh = (struct m_triangle*)malloc(sizeof(struct m_triangle) * recvNum); 
+		
+		sock.recvFrom(&mesh, recvNum, sourceAddress, sourcePort);
+
+		// recvString[bytesRcvd] = '\0';  // Terminate string
+    
+        cout << "Received " << recvNum << " from " << sourceAddress << ": "
+             << sourcePort << endl;
+
+		for(int ii = 0; ii < recvNum; ii++){
+			std::cout << mesh[ii].vertices[ii][0] << std::endl;
+		}
+		
+//    }
+  } catch (SocketException &e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+
+  return 0;
+}
---
+++ Network/Sender/data.hh
@@ -0,0 +1,33 @@
+// 
+//  data.hpp
+//  P7_2
+//  
+//  Created by Timothy Luciani on 2011-04-17.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// 
+
+#ifndef SERIALIZATION_DATA_HPP
+#define SERIALIZATION_DATA_HPP
+
+#include <string>
+
+/// Structure to hold information about a single triangle.
+struct m_triangle
+{
+	float vertex_normals[3][4]; /* vertex normals */
+//	int t[3]; /* texture coordinates */ 
+	float vertices[3][4]; /* 3 coordinates describing the triangle */
+};
+
+struct m_float4 {
+	
+	/* custom float 4 */
+	
+	float x;
+	float y;
+	float z;
+	float w;
+		
+};
+
+#endif // SERIALIZATION_DATA_HPP

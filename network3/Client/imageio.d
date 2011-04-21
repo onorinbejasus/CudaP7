939c1bbc6ab975377b349d8b74d9c3d66cf488fc
Timothy Luciani <tbl8@pitt.edu>
2011-04-20 20:54:53 -0400

    got it

---
+++ network3/Client/imageio.d
@@ -0,0 +1,45 @@
+imageio.o : imageio.cc \
+    /usr/X11/include/pngconf.h \
+    /usr/include/mach/i386/_structs.h \
+    /usr/include/machine/types.h \
+    /usr/include/sys/cdefs.h \
+    /usr/include/setjmp.h \
+    /usr/include/sys/signal.h \
+    /usr/include/machine/endian.h \
+    /usr/include/i386/endian.h \
+    /usr/include/AvailabilityInternal.h \
+    /usr/include/sys/_select.h \
+    /usr/include/i386/types.h \
+    /usr/X11/include/png.h \
+    /usr/include/stdio.h \
+    /usr/include/i386/_structs.h \
+    imageio.hh \
+    /usr/include/stdlib.h \
+    /usr/include/sys/_structs.h \
+    /usr/include/_structs.h \
+    /usr/include/sys/select.h \
+    /usr/include/sys/_types.h \
+    /usr/include/Availability.h \
+    /usr/include/_types.h \
+    /usr/include/sys/wait.h \
+    /usr/include/sys/_endian.h \
+    /usr/include/sys/unistd.h \
+    /usr/include/libkern/i386/_OSByteOrder.h \
+    /usr/include/unistd.h \
+    /usr/include/machine/setjmp.h \
+    /usr/include/machine/signal.h \
+    /usr/include/i386/setjmp.h \
+    /usr/local/include/zlib.h \
+    /Users/timothyluciani/Dropbox/Document/Classes/Spring\ 2011/CMU\ CUDA/Projects/Project7/CudaP7/network3/Client// \
+    /usr/include/i386/signal.h \
+    /usr/include/sys/appleapiopts.h \
+    /usr/include/libkern/_OSByteOrder.h \
+    /usr/include/string.h \
+    /usr/include/sys/resource.h \
+    /usr/include/time.h \
+    /usr/include/alloca.h \
+    /usr/include/machine/_structs.h \
+    /usr/include/machine/_types.h \
+    /usr/include/i386/_types.h \
+    /usr/local/include/zconf.h \
+    /usr/include/sys/types.h
